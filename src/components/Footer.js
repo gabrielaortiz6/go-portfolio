a9376851c928e6e5d7edbb54db1b0d41d16e05da
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-11-20 22:56:16 -0500

    link font size change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,22 +8,22 @@ const Footer = () => {
       <Container className="mt-4" style={{ fontStyle: 'italic'}}>
         <Row style={{ margin: '0px', padding: '0px'}}>
           <Col style={{ margin: '0px'}}>
-            <a style={{fontSize:'20px'}}href="https://github.com/gabrielaortiz6/" target="_blank" className="link-container" rel="noopener noreferrer">
+            <a style={{fontSize:'22px'}}href="https://github.com/gabrielaortiz6/" target="_blank" className="link-container" rel="noopener noreferrer">
               (github)
             </a>
           </Col>
           <Col>
-            <a href="https://www.linkedin.com/in/gabriela-ortiz-74641518a/" className="link-container" target="_blank" rel="noopener noreferrer">
+            <a style={{fontSize:'22px'}} href="https://www.linkedin.com/in/gabriela-ortiz-74641518a/" className="link-container" target="_blank" rel="noopener noreferrer">
              (linkedin)
             </a>
           </Col>
           <Col>
-            <a href="https://www.instagram.com/mieldeabeja.art/" className="link-container" target="_blank" rel="noopener noreferrer">
+            <a style={{fontSize:'22px'}} href="https://www.instagram.com/mieldeabeja.art/" className="link-container" target="_blank" rel="noopener noreferrer">
              (instagram)
             </a>
           </Col>
           <Col>
-            <a href={`mailto:${emailAddress}`} className="link-container" target="_blank" rel="noopener noreferrer">
+            <a style={{fontSize:'22px'}} href={`mailto:${emailAddress}`} className="link-container" target="_blank" rel="noopener noreferrer">
              (email)
             </a>
           </Col>
